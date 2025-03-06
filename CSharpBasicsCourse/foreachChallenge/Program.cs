aeb2ad8cf559f54d65ef7b89cf12c2fe8eda0c1a
ValeriyaMix <lerka-mishka@yandex.ru>
2025-03-06 18:21:57 +0000

    Solution to the Challenge 1: foreach.

---
+++ CSharpBasicsCourse/foreachChallenge/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+using foreachChallenge;
+
+List<string> namesList = new List<string>() { "Val Mik", "Doug The", "Jason La" };
+
+//foreach (string name in namesList)
+//{
+//    Console.WriteLine(name);
+//}
+
+
+List<PersonData> personList = new List<PersonData>()
+{
+    new PersonData{ FirstName = "Val", LastName = "Mik" },
+    new PersonData{ FirstName = "Mike", LastName = "P" },
+    new PersonData{ FirstName = "Sam", LastName = "Wilson" },
+
+};
+
+foreach (var person in personList)
+{
+    Console.WriteLine($"{person.FirstName} {person.LastName}");
+}
+
